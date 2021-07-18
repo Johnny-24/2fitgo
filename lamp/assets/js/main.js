c7d8c0b90daaf5598bc9a2d25f7944b405ec9ab2
Евгений Кудрявцев <johnny@MacBook-Air-Evgenij.local>
2021-07-19 00:07:06 +0500

    add project lamp

---
+++ lamp/assets/js/main.js
@@ -0,0 +1,51 @@
+"use strict";
+
+$(document).on("ready", function () {
+  // Svg for everybody
+  svg4everybody(); // Polifill for IE objectFitImages
+
+  objectFitImages(); // Category
+
+  var cat_item = $(".category-menu__item ");
+
+  function catToggle() {
+    $(cat_item).removeClass("active");
+    $(this).addClass("active");
+  }
+
+  $(cat_item).on("click", catToggle); // Detail images
+
+  var preview_img = $(".detail-main__preview-img");
+
+  function selectPrevPhoto() {
+    var slide_src = $(this).eq(0).attr("src");
+    $(".detail-main__img").attr("src", slide_src);
+  }
+
+  if (preview_img.length) {
+    var slide_src = $(preview_img).eq(0).attr("src");
+    $(".detail-main__img").attr("src", slide_src);
+    $(preview_img).on("click", selectPrevPhoto);
+  } //Mobile menu
+
+
+  var burger = $('.burger');
+  var body = $('body');
+  $(burger).on('click', function () {
+    $(body).toggleClass('active');
+  }); //modal
+
+  var modal_btn = $('.open-modal');
+  var modal_close = $('.close-modal');
+  $(modal_btn).on('click', function () {
+    var id = $(this).attr('href');
+    $(id).addClass('active');
+  });
+  $(modal_close).on('click', function () {
+    $('.modal').removeClass('active');
+  });
+
+  function modalSuccess() {
+    $('.modal').removeClass('active');
+  }
+});
