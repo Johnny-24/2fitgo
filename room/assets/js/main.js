bb8dac64c55fddb877810f4290d002ea00f2e7ef
Johnny <dos_24@inbox.ru>
2021-07-22 09:23:53 +0500

    room

--- room/assets/js/main.js
+++ room/assets/js/main.js
@@ -4,82 +4,15 @@ $(document).on('ready', function () {
   // Svg for everybody
   svg4everybody(); // Polifill for IE objectFitImages
 
-  objectFitImages(); // scroll main block
-  // let mainTitle = $('.main-section__title');
-  // let hScreen = $('.main-section').height();
-  // let hMainTitle = $(mainTitle).height();
-  // let mainPhotos = $('.main-photos__item');
-  // let footer = $('.footer');
-  // let footerSecond = $('.footer-second');
-  // let cursorElBlock = $('.main-photos__item').eq(-1);
-  // let cursorElBlockH = $(cursorElBlock).offset().top - hScreen;
-  // let lastScreenPosition = $('.main-photos__item').eq(-1);
-  // let endPOsition = $(lastScreenPosition).offset().top + $(lastScreenPosition).height();
-  // let scrollToDownFlag = true
-  // let lastPhotoPosition = ($(mainPhotos).eq(mainPhotos.length - 2).offset().top) + hMainTitle;
-  // let indent = hScreen - ((hScreen / 2) + (hMainTitle / 2));
-  //     if(pageYOffset < indent || pageYOffset > lastPhotoPosition) {
-  //         $(mainTitle).removeClass('active');
-  //     } else {
-  //         $(mainTitle).addClass('active');
-  //     }
-  //     if(pageYOffset > lastPhotoPosition) {
-  //         $(footerSecond).addClass('active');
-  //         $(footer).addClass('hide');
-  //     } else {
-  //         $(footerSecond).removeClass('active');
-  //         $(footer).removeClass('hide');
-  //     }
-  //     if(pageYOffset > (cursorElBlockH + (hScreen / 2)) && scrollToDownFlag) {
-  //         scrollToDownFlag = false
-  //         $("html, body").animate({ scrollTop: endPOsition }, 1000);
-  //         $('.custom-cursor').css('display', 'flex');
-  //         $('html').css('cursor', 'none');
-  //     } else if (pageYOffset < (cursorElBlockH + (hScreen / 2)) && !scrollToDownFlag) {
-  //         scrollToDownFlag = true
-  //         $('.custom-cursor').hide();
-  //         $('html').css('cursor', 'default');
-  //     }
-  //     let footer_second_left = $('.footer-second__left');
-  //     $(footer_second_left).on('mouseleave', function() {
-  //         $('.custom-cursor').css('display', 'flex');
-  //         $('html').css('cursor', 'none');
-  //     });
-  //     $(footer_second_left).on('mouseover', function() {
-  //         $('.custom-cursor').hide();
-  //         $('html').css('cursor', 'default');
-  //     });
-  // });
-  // Cursor
-  // let cursor = $('.custom-cursor');
-  // function mouseX(event) {
-  //     return event.clientX
-  // };
-  // function mouseY(event) {
-  //     return event.clientY
-  // }
-  // function positionElement(event) {
-  //     let mouse = {
-  //         x: mouseX(event),
-  //         y: mouseY(event)
-  //     }
-  //     $(cursor).css('top', (mouse.y - $(cursor).height() / 2) + 'px');
-  //     $(cursor).css('left', (mouse.x - $(cursor).width() / 2) + 'px');
-  // }
-  // window.onmousemove = function(event) {
-  //     let _event = event
-  //     setTimeout(function () {
-  //         positionElement(_event)
-  //     }, 1)
-  // }
+  objectFitImages(); // GSAP scroll effect
 
-  var skewSetter = gsap.quickSetter(".main-photos__item", "skewY", "deg");
+  var skewSetter = gsap.quickSetter(".it", "skewY", "deg");
   var proxy = {
     skew: 0
   };
   ScrollTrigger.create({
     onUpdate: function onUpdate(self) {
-      var skew = self.getVelocity() / -300;
+      var skew = self.getVelocity() / -1000;
 
       if (Math.abs(skew) > Math.abs(proxy.skew)) {
         proxy.skew = skew;
@@ -94,7 +27,90 @@ $(document).on('ready', function () {
         });
       }
     }
-  });
-}); //100% = 573
-//60% = 344
-//1% = 5.7
+  }); // scroll main block
+
+  var mainTitle = $('.main-section__title');
+  var hScreen = $('.main-section').height();
+  var hMainTitle = $(mainTitle).height();
+  var mainPhotos = $('.main-photos__item');
+  var footer = $('.footer');
+  var footerSecond = $('.footer-second');
+  var cursorElBlock = $('.main-photos__item').eq(-1);
+  var cursorElBlockH = $(cursorElBlock).offset().top - hScreen;
+  var lastScreenPosition = $('.main-photos__item').eq(-1);
+  var endPOsition = $(lastScreenPosition).offset().top + $(lastScreenPosition).height();
+  var scrollToDownFlag = true;
+  var lastPhotoPosition = $(mainPhotos).eq(mainPhotos.length - 2).offset().top + hMainTitle;
+  var indent = hScreen - (hScreen / 2 + hMainTitle / 2);
+  $(window).on('scroll', function () {
+    // title
+    if (pageYOffset < indent || pageYOffset > lastPhotoPosition) {
+      $(mainTitle).removeClass('active');
+    } else {
+      $(mainTitle).addClass('active');
+    } //footer
+
+
+    if (pageYOffset > lastPhotoPosition) {
+      $(footerSecond).addClass('active');
+      $(footer).addClass('hide');
+      $('.main-photos__item').addClass('fly');
+    } else {
+      $(footerSecond).removeClass('active');
+      $(footer).removeClass('hide');
+      $('.main-photos__item').removeClass('fly');
+    }
+
+    if ($(window).width() > 768) {
+      if (pageYOffset > cursorElBlockH + hScreen / 2 && scrollToDownFlag) {
+        scrollToDownFlag = false;
+        $('.custom-cursor').css('display', 'flex');
+        $('html').css('cursor', 'none');
+      } else if (pageYOffset < cursorElBlockH + hScreen / 2 && !scrollToDownFlag) {
+        scrollToDownFlag = true;
+        $('.custom-cursor').hide();
+        $('html').css('cursor', 'default');
+      }
+
+      var footer_second_left = $('.footer-second__left');
+      $(footer_second_left).on('mouseleave', function () {
+        $('.custom-cursor').css('display', 'flex');
+        $('html').css('cursor', 'none');
+      });
+      $(footer_second_left).on('mouseover', function () {
+        $('.custom-cursor').hide();
+        $('html').css('cursor', 'default');
+      });
+    }
+
+    ;
+  }); // Cursor
+
+  var cursor = $('.custom-cursor');
+
+  function mouseX(event) {
+    return event.clientX;
+  }
+
+  ;
+
+  function mouseY(event) {
+    return event.clientY;
+  }
+
+  function positionElement(event) {
+    var mouse = {
+      x: mouseX(event),
+      y: mouseY(event)
+    };
+    $(cursor).css('top', mouse.y - $(cursor).height() / 2 + 'px');
+    $(cursor).css('left', mouse.x - $(cursor).width() / 2 + 'px');
+  }
+
+  window.onmousemove = function (event) {
+    var _event = event;
+    setTimeout(function () {
+      positionElement(_event);
+    }, 1);
+  };
+});
