d0d0c72bc9cb4a0e74abbbe384a3ba05e0abf6b2
Johnny <dos_24@inbox.ru>
2023-10-10 23:01:07 +0500

    a

---
+++ pages/signUp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">
+    <AppHeader />
+    <div class="page">
+      <h1>Регистрация</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.page {
+  padding: 50px;
+  text-align: center;
+}
+</style>
